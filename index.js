c97c5ee83ef30fc9cfa619a816e83f2e7757c8ec
Denis Kibichiy <deniskibichiy@outlook.com>
2025-05-23 23:08:39 +0300

    create the square divs

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const div = document.querySelector("#container");
+div.classList.add('div');
+
+let divsNum = 256;
+let i;
+
+for (i = 0; i <= divsNum; i++) {
+    const divChild = document.createElement("div");
+    divChild.classList.add("divChild");
+    div.appendChild(divChild);
+
+const squareDivs = document.createElement('div');
+};
