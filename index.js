cc70537b10f23362581df626df19b65d23e59208
Denis Kibichiy <deniskibichiy@outlook.com>
2025-05-24 20:43:52 +0300

    change code to create columns first

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const div = document.querySelector("#container");
 div.classList.add('div');
 
-let divsNum = 256;
+let divsNum = 16;
 let i;
 
+
+// let the number of rows be 16
+//append the columns to each row
+//append each row to the parent container
 for (i = 0; i <= divsNum; i++) {
     const divChild = document.createElement("div");
     divChild.classList.add("divChild");
     div.appendChild(divChild);
-
-const squareDivs = document.createElement('div');
+    const columnDivs = document.createElement('div');
+    columnDivs.classList.add("columnDivs");
+    divChild.appendChild(columnDivs);
 };
