587dd1565cb0836707caa67c81c9dd1aefa4deab
Denis Kibichiy <deniskibichiy@outlook.com>
2025-06-23 21:04:06 +0300

    make the width of the container fixed

--- index.js
+++ index.js
@@ -3,21 +3,23 @@ function createSquareDivs (numberPerRow) {
 
     const container_div = document.querySelector('.container');
     const total = (numberPerRow * numberPerRow) + numberPerRow;
-    const mod = numberPerRow + 1
+    const mod = numberPerRow + 1;
+    container_div.style.cssText ="width: 500px; height: 500px"
 
 
     for (let i = 1; i < total; i++ ) {
         const columnsDivs = document.createElement('div');
+        
 
         if (i % mod === 0) {
             columnsDivs.style.cssText = "border: 0; height: 0; width: 100%;"
         } else {
-            columnsDivs.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+            const unit = "px";
+            const width = 500/numberPerRow;
+            columnsDivs.style.cssText = `border: 1px solid black; height: ${width}${unit}; width: ${width}${unit}`;
         }
          container_div.appendChild(columnsDivs);
     }
-   
-    
 }
 
-createSquareDivs(16);
+createSquareDivs(30);
