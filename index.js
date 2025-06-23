8dfe6da3c31e16e0e365ad1748a1f6ec7c785a26
Denis Kibichiy <deniskibichiy@outlook.com>
2025-06-23 20:37:21 +0300

    convert div creation into a function

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const container_div = document.querySelector("#container");
 
-let divsNum = 257;
+function createSquareDivs (numberPerRow) {
 
-function createSquareDivs () {
-    for (let i = 0; i < 273; i++ ) {
+    const container_div = document.querySelector('.container');
+    const total = (numberPerRow * numberPerRow) + numberPerRow;
+    const mod = numberPerRow + 1
+
+
+    for (let i = 1; i < total; i++ ) {
         const columnsDivs = document.createElement('div');
-        columnsDivs.classList.add("square_div");
-        container_div.appendChild(columnsDivs);
+
+        if (i % mod === 0) {
+            columnsDivs.style.cssText = "border: 0; height: 0; width: 100%;"
+        } else {
+            columnsDivs.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+        }
+         container_div.appendChild(columnsDivs);
     }
+   
     
 }
 
-createSquareDivs();
+createSquareDivs(16);
